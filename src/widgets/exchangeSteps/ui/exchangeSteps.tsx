3955b7560f6059be6383055d4c8b6b9acb3547b6
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-12-08 22:49:27 +0300

    feat: Добавлен адаптив для секции exchange

--- src/widgets/exchangeSteps/ui/exchangeSteps.tsx
+++ src/widgets/exchangeSteps/ui/exchangeSteps.tsx
@@ -1,24 +1,48 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { STEPS } from "../config";
 import { MotionExchangeStep } from "./exchangeStep";
-import styles from "./exchangeSteps.module.scss";
 import { useExchangeStepsAnim } from "../model/useExchangeStepsAnim";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import styles from "./exchangeSteps.module.scss";
+import "swiper/swiper-bundle.css";
 
 export const ExchangeSteps: FC = () => {
   const { ref, rotates, translateY } = useExchangeStepsAnim();
+  const paginationRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className={styles.exchangeSteps} ref={ref}>
-      {STEPS.map((step, index) => (
-        <MotionExchangeStep
-          key={index}
-          step={step}
-          style={{
-            rotate: rotates[index],
-            translateY: translateY,
-          }}
-        />
-      ))}
+      <Swiper
+        modules={[Pagination]}
+        className={styles.stepsSlider}
+        pagination={{
+          clickable: true,
+          bulletActiveClass: styles.bulletActive,
+          bulletClass: styles.bullet,
+        }}
+        slidesPerView={1}
+        spaceBetween={24}
+        freeMode
+        breakpoints={{
+          767.8: {
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {STEPS.map((step, index) => (
+          <SwiperSlide className={styles.slide} key={index}>
+            <MotionExchangeStep
+              step={step}
+              style={{
+                rotate: rotates[index],
+                translateY: translateY,
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={styles.swiperPagination} ref={paginationRef}></div>
     </div>
   );
 };
